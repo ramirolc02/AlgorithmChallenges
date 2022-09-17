c33a22d77f32a5f23d3165977e41ce31b05f1700
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-09-17 17:46:34 +0200

    pijote java done

--- ex435/pijote.java
+++ ex435/pijote.java
@@ -1,5 +1,32 @@
 package ex435;
+import java.io.BufferedInputStream;
+import java.util.Scanner;
 
 public class pijote {
-    
+
+    private static boolean no_subnormal(int[] contador){
+        boolean aux = true;
+        int j=0;
+        while(aux && j < 9){
+            aux = contador[j] == contador[j+1];
+            j++;
+        }
+        return aux;
+    }
+    public static void main(String[]args){
+        try(Scanner sc = new Scanner(new BufferedInputStream(System.in, 1*1024))) {
+            while(sc.hasNextLine()){
+            char[] cadena = sc.nextLine().toCharArray();
+            int size = cadena.length;
+            int contador [] = new int[10];
+            for(int i =0; i < size; i++){
+                contador[Integer.parseInt(String.valueOf(cadena[i]))]++;
+            }
+            String salida = (no_subnormal(contador)) ? "subnormal" : "no subnormal";
+            System.out.println(salida);
+        }
+        } catch (Exception e) {
+           System.exit(1);
+        }
+    }
 }
