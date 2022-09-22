35a1807e27cc3b59bbc30e67f08c983cefe57baa
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-09-22 23:58:39 +0200

    contar.java mejor version

--- ex441/contar.java
+++ ex441/contar.java
@@ -5,35 +5,29 @@ import java.util.Scanner;
 
 public class contar {
     public static String suma( char[] cadena) {
-		int sum=0;
 		int i = cadena.length - 1;
 		while(i > -1) {
-			if(cadena[i] == '.') {
-				i--;
-			}
-			else if( cadena[i]=='9') 
-			{
-                cadena[i] = '0';
-				i--;
-			}
-			else  {
-                sum = Character.getNumericValue(cadena[i]) + 1;
-				cadena[i] = (char) (sum+'0');
-				break;
-			}
+			if(cadena[i] != '.') {
+			        if( cadena[i]=='9') {
+                        cadena[i] = '0';
+                        if(i==0) {
+                            if(cadena.length == 3){
+                                return "1." + String.valueOf(cadena);
+                            }
+                            else if(cadena.length > 4 && cadena[3]=='.'){
+                                return "1." + String.valueOf(cadena);
+                            }
+                            else{
+                                return "1" + String.valueOf(cadena);
+                            }
+                        }
+                    }else  {
+                       cadena[i]++;
+                       break;
+			        }
+                }
+            i--;
 		}
-		if(sum==0) {
-            if(cadena.length == 3){
-                return "1." + String.valueOf(cadena);
-            }
-            else if(cadena.length > 4 && cadena[3]=='.'){
-                return "1." + String.valueOf(cadena);
-            }
-            else{
-                return "1" + String.valueOf(cadena);
-            }
-        }
-		
 		return String.valueOf(cadena);
 	}
 	public static void main(String[] args) {
@@ -49,7 +43,6 @@ public class contar {
 		catch(Exception e) {
 			System.exit(1);
 		}
-
 	}
 }
 
