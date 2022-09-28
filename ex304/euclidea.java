05195febb6567de2f79121ca6c70af40289f2c49
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-09-29 00:27:28 +0200

    euclidea

--- ex304/euclidea.java
+++ ex304/euclidea.java
@@ -5,28 +5,37 @@ import java.util.Scanner;
 
 public class euclidea {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(new BufferedInputStream(System.in, 1 * 1024))) {
-            int casos = sc.nextInt();
-
-            while (casos > 0) {
-                int dividendo = sc.nextInt();
-                int divisor = sc.nextInt();
-                System.err.println(String.format("%d / %d", dividendo, divisor));
-
-                if (divisor == 0) {
-                    System.out.println("DIV0");
-                } else if (dividendo == 0) {
-
-                } else if (dividendo > 0 && divisor > 0) {
-
-                } else {
-
-                }
-                --casos;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+		try(Scanner s = new Scanner(new BufferedInputStream(System.in,1*1024)))
+		{
+			  int n,a,b,cociente=0,resto=0;
+			  n=s.nextInt();
+			  for (;n>0;n--){
+				 a=s.nextInt();
+				 b=s.nextInt();
+			    if(b==0)
+			    {
+			      System.out.println("DIV0");
+			      continue;
+			    }
+			    resto=a%b;
+			    cociente=a/b;
+			    if(resto < 0) {
+			    	if(b<=0) {
+			    		cociente++;
+			    	}
+			    	else {
+			    		cociente--;
+			    	}
+			    	resto=a-b*cociente;
+			    }
+			    	    
+			    System.out.println(cociente +" "+ resto);
+			    cociente=0;
+			    resto=0;
+			}
+		}
+		catch(Exception e){
+			System.exit(1);
+		}
+	}
 }
