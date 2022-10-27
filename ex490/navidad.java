a85ed1c63788c907d962c8d2fd9e70e43b7c7f0c
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-10-27 21:39:35 +0200

    LucesNavidad Done

--- ex490/navidad.java
+++ ex490/navidad.java
@@ -1,25 +1,20 @@
 package ex490;
 import java.io.BufferedInputStream;
 import java.util.Scanner;
+import java.math.*;
 
 public class navidad {
     public static int gradoSatisfaccion( int [] arr, int k){
         int max = 0;
         for (int i = 0; i < arr.length-k+1;i++){
-            if (arr[i] > arr[i+k-1] && max < (arr[i] - arr[i+k-1])){
-                max = arr[i] - arr[i+k-1];
+            if (max < Math.abs(arr[i] - arr[i+k-1])){
+                max = Math.abs(arr[i] - arr[i+k-1]);
             }
-            else if (arr[i] < arr[i+k-1] && max < (arr[i+k-1] - arr[i])){
-                max =  arr[i+k-1] - arr[i];  
-            } 
             }
             return max ;
         }
-        
     public static void main(String[] args) {
-
         try (Scanner sc = new Scanner(new BufferedInputStream(System.in, 1*1024))) {
-
         while(sc.hasNext()){
            int n = sc.nextInt(); // total
            int m = sc.nextInt(); // recorrer
@@ -28,17 +23,11 @@ public class navidad {
            for (int i = 0; i < n; i++) {
                 array[i] = sc.nextInt();
            }
-
            System.out.println(gradoSatisfaccion(array, m));
-
             }
-
-
-
            }
      catch (Exception e) {
            System.exit(1);
        } 
    } 
-    
 }
--- ex577/mafalda.java
+++ ex577/mafalda.java
@@ -1,54 +1,52 @@
 package ex577;
+
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 
 public class mafalda {
-    
+
     public static void main(String[] args) {
 
-        try (Scanner sc = new Scanner(new BufferedInputStream(System.in, 1*1024))) {
-           int n = sc.nextInt(); // casos
-           int contador = 0;
-           while( n > 0){
-            contador = 0;
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            if (a + b == c){
-                    while( a != c+1 && b != -1){
+        try (Scanner sc = new Scanner(new BufferedInputStream(System.in, 1 * 1024))) {
+            int n = sc.nextInt(); // casos
+            int contador = 0;
+
+            while (n > 0) {
+                contador = 0;
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int c = sc.nextInt();
+                if (a + b == c) {
+                    while (a != c + 1 && b != -1) {
                         contador++;
                         a++;
                         b--;
                     }
                     System.out.println(contador);
-                }
-            else if (a + b < c){
-                 a = c-b;
-                 while( a != c+1 && b != -1){
-                    contador++;
-                    a++;
-                    b--;
-                }
-                System.out.println(contador);
-            }
-            else{
-                while( a != c+1 && b != -1){
-                    b = c-a;
-                    while( a != c+1 && b != -1){
+                } else if (a + b < c) {
+                    a = c - b;
+                    while (a != c + 1 && b != -1) {
                         contador++;
                         a++;
                         b--;
                     }
                     System.out.println(contador);
+                } else {
+                    while (a != c + 1 && b != -1) {
+                        b = c - a;
+                        while (a != c + 1 && b != -1) {
+                            contador++;
+                            a++;
+                            b--;
+                        }
+                        System.out.println(contador);
+                    }
                 }
-            }
-            n--;
+                n--;
             }
 
-           }
-     catch (Exception e) {
-           System.exit(1);
-       } 
-   } 
+        } catch (Exception e) {
+            System.exit(1);
+        }
+    }
 }
-
